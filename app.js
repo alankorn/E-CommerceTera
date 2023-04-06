bd22872bea1074bfea7c27cb467c0b4dfd99a882
Alan Ferreira <ti026@pacaembuautopecas.com.br>
2023-04-06 10:23:22 -0300

    Cadastro funcionando

---
+++ app.js
@@ -0,0 +1,25 @@
+let users = []
+
+function criaUser(){
+    var name = document.querySelector("#inputNome").value
+    var lastName = document.querySelector("#inputSobrenome").value
+    var cpf = document.querySelector("#inputCPF").value
+    var email = document.querySelector("#inputEmailCadastro").value
+    var password = document.querySelector("#senhaCadastroInput").value
+    var passwordConfirm = document.querySelector("#senhaConfirmacaoCadastroInput").value
+    
+    var newUser = new Object()
+    newUser.nameUser = name;
+    newUser.lastNameUser = lastName;
+    newUser.cpfUser = cpf;
+    newUser.emailUser = email;
+    if(password == passwordConfirm){
+        newUser.userPassword = password
+        users.push(newUser)
+    }
+    else{
+        document.querySelector("#senha_nao_corresponde").innerHTML = 'Senha não corresponde'
+    }
+
+    
+}
