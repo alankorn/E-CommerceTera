bc6d5b74683d35848d5e32ecc54318fdf5f4a149
Alunos <Alunos@USJ12DTL6801417.animaedu.intranet>
2023-04-20 20:17:55 -0300

    Alteração de erro

--- LoginCadastro/app.js
+++ LoginCadastro/app.js
@@ -1,42 +1,37 @@
-let users = []
+let users = [];
 
-function criaUser(){
-    var name = document.querySelector("#inputNome").value
-    var lastName = document.querySelector("#inputSobrenome").value
-    var cpf = document.querySelector("#inputCPF").value
-    var email = document.querySelector("#inputEmailCadastro").value
-    var password = document.querySelector("#senhaCadastroInput").value
-    var passwordConfirm = document.querySelector("#senhaConfirmacaoCadastroInput").value
-    var cep = document.querySelector("#inputCEP").value
-    var rua = document.querySelector("#inputRua").value
-    var bairro = document.querySelector("#inputBairro").value
-    var numero = document.querySelector("#inputNumero").value
-    var complemento = document.querySelector("#inputComplement").value
-    
-    var newUser = new Object()
-    newUser.nameUser = name;
-    newUser.lastNameUser = lastName;
-    newUser.cpfUser = cpf;
-    newUser.emailUser = email;
-    newUser.cepUser = cep;
-    newUser.ruaUser = rua;
-    newUser.bairroUser = bairro;
-    newUser.numeroUser = numero;
-    newUser.complementoUser = complemento;
-    
-    
-    if(password == passwordConfirm){
-        newUser.userPassword = password
-        users.push(newUser)
-    }
-    else{
-        document.querySelector("#senha_nao_corresponde").innerHTML = 'Senha não corresponde'
-    }
+function criaUser() {
+  var name = document.querySelector("#inputNome").value;
+  var lastName = document.querySelector("#inputSobrenome").value;
+  var cpf = document.querySelector("#inputCPF").value;
+  var email = document.querySelector("#inputEmailCadastro").value;
+  var password = document.querySelector("#senhaCadastroInput").value;
+  var passwordConfirm = document.querySelector(
+    "#senhaConfirmacaoCadastroInput"
+  ).value;
+  var cep = document.querySelector("#inputCEP").value;
+  var rua = document.querySelector("#inputRua").value;
+  var bairro = document.querySelector("#inputBairro").value;
+  var numero = document.querySelector("#inputNumero").value;
+  var complemento = document.querySelector("#inputComplement").value;
 
-    
+  var newUser = new Object();
+  newUser.nameUser = name;
+  newUser.lastNameUser = lastName;
+  newUser.cpfUser = cpf;
+  newUser.emailUser = email;
+  newUser.cepUser = cep;
+  newUser.ruaUser = rua;
+  newUser.bairroUser = bairro;
+  newUser.numeroUser = numero;
+  newUser.complementoUser = complemento;
+
+  if (password == passwordConfirm) {
+    newUser.userPassword = password;
+    users.push(newUser);
+    console.log(users);
+  } else {
+    document.querySelector("#senha_nao_corresponde").innerHTML =
+      "Senha não corresponde";
+  }
 }
-
-
-    
-
-
